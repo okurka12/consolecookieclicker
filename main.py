889bec4bdc3b3ee4b81053e7b67c35b4475055ba
okurka12 <pavlikvitek13@gmail.com>
2022-06-13 22:59:20 +0200

    made a small version with os.system('cls')

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
+
+
+class GameState:
+    def __init__(self):
+        self.cookies = 0
+
+    def addcookie(self):
+        self.cookies += 1
+
+
+def print_game_state(state: GameState) -> None:
+    print(f"Počet sušenek: {state.cookies}")
+
+
+def clear():
+    os.system("cls")
+
+
 def main():
     print("welcome")
 
