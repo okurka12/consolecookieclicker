2a64a6b17b6f5bc711148e5cc1f889452700a460
okurka12 <pavlikvitek13@gmail.com>
2022-06-14 05:13:34 +0200

    indicated how i want to continue developing this project but as of right now i feel to tired do carry on coding

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import os
 
+CLICKERS = [  # (cps, price)
+    (1, 15),
+    (10, 150)
+]
+
+
+GAME_UI = """Počet sušenek: $COOKIES
+CPS: $CPS
+Nakoupená klikátka:
+$CLICKERS
+o - obchod
+"""
+
+SHOP_UI = """
+
+"""
+
 
 class GameState:
     def __init__(self):
         self.cookies = 0
+        self.cps = 0
+        self.clickers = {}  # key: value -> cps: number bought
 
     def addcookie(self):
         self.cookies += 1
 
 
 def print_game_state(state: GameState) -> None:
-    print(f"Počet sušenek: {state.cookies}")
+    text = GAME_UI.replace("$COOKIES", str(state.cookies))
+    text = text.replace("$CPS", str(state.cps))
+    text = text.replace("$CLICKERS", "\n".join([f"{cps}cps clicker: {n}" for cps, n in state.clickers.items()]))
+    print(text)
 
 
 def clear():
@@ -28,7 +50,6 @@ def game_loop():
 
 def main():
     game_loop()
-
 
 
 if __name__ == "__main__":
